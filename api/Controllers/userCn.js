c0e33c4ff42b326557fa375a24ce276135e6f30d
Frolic7 <a.alereza50@gmail.com>
2025-02-05 09:15:10 +0330

    Compelete userCn

--- api/Controllers/userCn.js
+++ api/Controllers/userCn.js
@@ -1,5 +1,17 @@
 import catchAsync from "../Utils/catchAsync.js"
 import User from "../Models/userMd.js";
-
-export const updateUser=catchAsync(async(req,res,next)=>{})
-export const getOneUser=catchAsync(async(req,res,next)=>{})
+export const getOneUser = catchAsync(async (req, res, next) => { 
+    const user = await User.findById(req.userId)
+    return res.status(200).json({
+        success: true,
+        message: "I Got You :D",
+        data: user
+    })})
+export const updateUser = catchAsync(async (req, res, next) => {
+    const user = await User.findByIdAndUpdate(req.userId, req.body, { new: true, runValidators: true })
+    return res.status(200).json({
+        success: true,
+        message: "I Changed You :D",
+        data: user
+    }) 
+})
